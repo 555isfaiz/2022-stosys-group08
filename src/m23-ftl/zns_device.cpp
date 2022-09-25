17962cdbd84d525374b17fb0731d318cb73441fb
zhaxzhax <44116759+zhaxzhax@users.noreply.github.com>
2022-09-25 19:06:57 +0200

    Update zns_device.cpp
    update the full merge call situation

--- src/m23-ftl/zns_device.cpp
+++ src/m23-ftl/zns_device.cpp
@@ -33,10 +33,10 @@ SOFTWARE.
 extern "C"
 {
 
-#define ENTRY_INVALID                   (1L << 63)
-#define address_2_zone(addr)            ((addr) / (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes)) + zns_dev_ex->log_zone_num_config
-#define address_2_offset(addr)          ((addr) % (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes) / zns_dev->lba_size_bytes)
-#define zone_2_address(zone_no)         (zone_no - zns_dev_ex->log_zone_num_config) * (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes)
+#define ENTRY_INVALID (1L << 63)
+#define address_2_zone(addr) ((addr) / (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes)) + zns_dev_ex->log_zone_num_config
+#define address_2_offset(addr) ((addr) % (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes) / zns_dev->lba_size_bytes)
+#define zone_2_address(zone_no) (zone_no - zns_dev_ex->log_zone_num_config) * (zns_dev_ex->blocks_per_zone * zns_dev->lba_size_bytes)
 #define EMPTY 1
 #define FULL 14
 
@@ -72,7 +72,6 @@ extern "C"
     int ss_nvme_device_io_with_mdts(uint64_t slba, void *buffer, uint64_t buf_size, bool read)
     {
         int ret;
-        
 
         uint64_t size_left = buf_size, ptr = 0, io_num, wp = slba, lba_num, mdts_size = zns_dev_ex->mdts, lba_size = zns_dev->lba_size_bytes;
         __u64 res;
@@ -98,7 +97,8 @@ extern "C"
     {
         nvme_root_t r;
         r = nvme_scan(NULL);
-        if (!r){
+        if (!r)
+        {
             printf("nvme_scan call failed with errno %d , null pointer returned in the scan call\n", -errno);
             return NULL;
         }
@@ -112,10 +112,14 @@ extern "C"
         void *membase;
         int fd;
 
-        nvme_for_each_host(r, h) {
-            nvme_for_each_subsystem(h, subsystem) {
-                nvme_subsystem_for_each_ctrl(subsystem, controller) {
-                    nvme_ctrl_for_each_ns(controller, nspace) {
+        nvme_for_each_host(r, h)
+        {
+            nvme_for_each_subsystem(h, subsystem)
+            {
+                nvme_subsystem_for_each_ctrl(subsystem, controller)
+                {
+                    nvme_ctrl_for_each_ns(controller, nspace)
+                    {
                         if (strcmp(nvme_ns_get_name(nspace), devicename) == 0)
                         {
                             snprintf(path, sizeof(path), "%s/device/device/resource0", nvme_ns_get_sysfs_dir(nspace));
@@ -128,13 +132,15 @@ extern "C"
 
     loop_end:
         fd = open(path, O_RDONLY);
-        if (fd < 0) {
+        if (fd < 0)
+        {
             perror("can't open pci resource ");
             return NULL;
         }
 
         membase = mmap(NULL, getpagesize(), PROT_READ, MAP_SHARED, fd, 0);
-        if (membase == MAP_FAILED) {
+        if (membase == MAP_FAILED)
+        {
             perror("can't do mmap for device ");
             membase = NULL;
         }
@@ -144,34 +150,35 @@ extern "C"
     }
 
     // see 5.15.2.2 Identify Controller data structure (CNS 01h)
-    uint64_t get_mdts_size(int fd, const char *devicename){
+    uint64_t get_mdts_size(int fd, const char *devicename)
+    {
         void *membase = mmap_registers(devicename);
         if (!membase)
             return -1;
         __u64 val;
 
         // reverse edian
-        __u32 *tmp = (__u32 *)(membase);    // since NVME_REG_CAP = 0, no need to change address
+        __u32 *tmp = (__u32 *)(membase); // since NVME_REG_CAP = 0, no need to change address
         __u32 low, high;
 
         low = le32_to_cpu(*tmp);
         high = le32_to_cpu(*(tmp + 1));
 
-        val = ((__u64) high << 32) | low;
+        val = ((__u64)high << 32) | low;
         __u8 *p = (__u8 *)&val;
-        uint32_t cap_mpsmin = 1 <<  (12 + (p[6] & 0xf));
+        uint32_t cap_mpsmin = 1 << (12 + (p[6] & 0xf));
 
         munmap(membase, getpagesize());
 
         struct nvme_id_ctrl ctrl;
         int ret = nvme_identify_ctrl(fd, &ctrl);
         if (ret != 0)
-        {   
-        perror("ss nvme id ctrl error ");   
+        {
+            perror("ss nvme id ctrl error ");
             return ret;
         }
         // return (1 << ctrl.mdts) * cap_mpsmin;        // Without QEMU Bug!
-        return (1 << (ctrl.mdts - 1)) * cap_mpsmin;     // With QEMU Bug!
+        return (1 << (ctrl.mdts - 1)) * cap_mpsmin; // With QEMU Bug!
     }
 
     int get_free_lz_num()
@@ -335,15 +342,6 @@ extern "C"
                 break;
             }
 
-            // for every data zone, update it.
-            for (uint64_t i = zns_dev_ex->log_zone_num_config; i < zns_dev->tparams.zns_num_zones; i++)
-            {
-                if (data_mapping.find(i) != data_mapping.end())
-                {
-                    full_merge(i, data_mapping[i]);
-                }
-            }
-
             // for the remaining data in the log zone, allocate empty data zone to it
             while (true)
             {
@@ -368,13 +366,19 @@ extern "C"
                 }
                 else
                 {
-                    int ret = allocate_data_zone(zone_no);
+                    int ret;
+                    if (data_mapping.find(zone_no) != data_mapping.end())
+                    {
+                        ret = full_merge(zone_no, data_mapping[zone_no]);
+                    }
+                    else
+                    {
+                        ret = allocate_data_zone(zone_no);
+                    }
                     if (ret)
                     {
-                        printf("allocate data zone error: %d! zone_no:%ld \n", ret, zone_no);
+                        printf("allocate data zone or full merge error: %d! zone_no:%ld \n", ret, zone_no);
                     }
-                    // else
-                        // printf("allocate data zone virtual number zone_no:%ld successfully!\n", zone_no);
                 }
             }
 
@@ -517,7 +521,7 @@ extern "C"
 
                 entry = data_mapping[zone_no] + address_2_offset(address);
             }
-            
+
             ret = nvme_read(info->fd, info->nsid, (entry & ~ENTRY_INVALID), 0, 0, 0, 0, 0, 0, lba_s, (char *)buffer + num_read, 0, NULL);
             if (ret)
             {
